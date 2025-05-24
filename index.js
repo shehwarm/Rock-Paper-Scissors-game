0986054c644ea1ce4485568dfd87f26a792f8816
SehwarMaryam <shehwarm93@gmail.com>
2025-05-24 19:56:06 +0500

    Fix case sensitivity in game logic and initialize score tracking

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const choices = ['rock', 'paper', 'scissors'];
 const playerDisplay = document.getElementById('playerDisplay');
 const computerDisplay = document.getElementById('computerDisplay');
 const resultDisplay = document.getElementById('resultDisplay');
+const playerScoreDisplay = document.getElementById('playerScoreDisplay');
+const computerScoreDisplay = document.getElementById('computerScoreDisplay');
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoice){
     const computerChoice = choices[Math.floor(Math.random()* 3)];
@@ -15,13 +20,13 @@ function playGame(playerChoice){
     else{
         switch(playerChoice){
             case "rock":
-               result = (computerChoice === "Scissors") ?  "YOU WIN!" : "YOU LOSE!";
+               result = (computerChoice === "scissors") ?  "YOU WIN!" : "YOU LOSE!";
                 break;
             case "paper":
-                result = (computerChoice === "Rock") ? "YOU WIN!" : "YOU LOSE!";
+                result = (computerChoice === "rock") ? "YOU WIN!" : "YOU LOSE!";
                 break;
             case "scissors":  
-                result = (computerChoice === "Paper") ? "YOU WIN!" : "YOU LOSE!";
+                result = (computerChoice === "paper") ? "YOU WIN!" : "YOU LOSE!";
                 break;
             
         }
@@ -30,12 +35,16 @@ function playGame(playerChoice){
     computerDisplay.textContent = `COMPUTER : ${computerChoice}`;
     resultDisplay.textContent = result;
 
-    resultDisplay.classList.remove("greenText", "redText");
+    resultDisplay.classList.remove("greenText", "redText", "yellowText");
 
     if (result === "YOU WIN!") {
         resultDisplay.classList.add("greenText");
+        playerScore++;
+        playerScoreDisplay.textContent = ` ${playerScore}`;
     } else if (result === "YOU LOSE!") {
         resultDisplay.classList.add("redText");
+        computerScore++;
+        computerScoreDisplay.textContent = ` ${computerScore}`;
     } else if(result === "It's a tie!") {
         resultDisplay.classList.add("yellowText");
     }
